ac741f043cb52902021ea800a5d4744f764bb076
ariskaAdi <ariskaadiprasetyo@gmail.com>
2025-09-12 22:28:22 +0700

    add hero

--- frontend/src/app/(main)/page.tsx
+++ frontend/src/app/(main)/page.tsx
@@ -1,14 +1,16 @@
-import PromoCard from "@/components/layouts/promo-carousel/PromoCard";
+import { HeroSection } from "@/components/layouts/hero/hero";
 import HomePage from "@/components/pages/Home";
+import DestinationCard from "@/components/layouts/destination-carousel/DestinationCard";
 
 export default function Home() {
   return (
     <div>
+      <HeroSection />
+      <div id="promo">
+        <DestinationCard />
+      </div>
       <div id="property">
         <HomePage />
-      </div>
-      <div id="promo">
-        <PromoCard />
       </div>
     </div>
   );
